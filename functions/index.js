9f78315429ab6dfa8d6ac7d97a7653e24b8b6acd
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-29 22:59:57 -0400

    deployed to firebase and created a GET route

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+import functions from 'firebase-functions'
+import express from 'express';
+import cors from 'cors';
+import { getHikingTrails }  from './src/hikingtrails.js'
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/hikingtrails', getHikingTrails)
+
+app.get('/test', (req, res) => res.send({ success: true, message: 'API is working!' }))
+
+export const api = functions.https.onRequest(app);
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
